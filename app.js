0b3bebb8ec9388a682f6246e9b6e10d4366a994d
sergiocp1997 <sergio.canadillas@alu.uclm.es>
2022-03-11 16:56:16 +0100

    adding connection

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://sergio:sergiomdb@cluster0.cqtoi.mongodb.net/mernflix?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() =>  console.log('mymerndb connection successful'))
+    .catch((err) => console.error(err));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
